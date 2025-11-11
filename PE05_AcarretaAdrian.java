f02bdf62a4acd06f0a056f92770dffc50ba19e7d
Adrian Acarreta <alu.adrian.acarreta@mataro.epiaedu.cat>
2025-11-11 17:54:19 +0100

    Control dexcepcions dels preuUnitat i quantitat controlats, afegit que si la comanda no es completa no s,imprimeixi i funció d,afegir productes acabada.

--- PE05_AcarretaAdrian.java
+++ PE05_AcarretaAdrian.java
@@ -11,8 +11,12 @@ public class PE05_AcarretaAdrian {
     final String VERDE = "\u001B[32m";
     final String AMARILLO = "\u001B[33m";
     final String AZUL = "\u001B[34m";
+    final int IVA = 10;
     String r; // VARIABLE PER INPUTS
-    
+    double totalProductes;
+    double IVAperAplicar;
+    double totalNet;
+
     Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
         PE05_AcarretaAdrian p = new PE05_AcarretaAdrian();
@@ -35,10 +39,10 @@ public class PE05_AcarretaAdrian {
                 comanda=crearComanda(comanda);
                 break;
             case "2":
-                //actualitzarComanda();
+                comanda=afegirProductes(comanda);
                 break;
             case "3":
-                //visualitzarTiquet();
+                imprimirTiquet(comanda);
                 break;
             case "4":
                 System.out.println(AMARILLO+"(-) Sortint del programa..."+RESET);
@@ -51,46 +55,69 @@ public class PE05_AcarretaAdrian {
     }
 
     public String crearComanda(String comanda) {
-        String producte;
         System.out.println("\n------------------------------------");
         System.out.println("=========== NOVA COMANDA ===========");
         System.out.println("------------------------------------\n");
         System.out.print(AMARILLO + "\n> Introdueix el nom del client: " + RESET);
         String nom = s.next();
-        comanda="\n------------------------------------\n============= TIQUET =============\n------------------------------------\nClient: "+nom+"\n\nProducte\tQuantitat\tPreu unit.\tSubtotal\n------------------------------------";
+        comanda="\n------------------------------------------------------------------------\n============================== TIQUET ==============================\n------------------------------------------------------------------------\nClient: "+nom+"\n\nProducte\tQuantitat\tPreu unit.\tSubtotal\n------------------------------------------------------------------------";
         r="n";
-        do {
-            s.nextLine();
-            producte = producteFormatat();
-            System.out.print(AMARILLO + "\n> Preu unitari (€): " + RESET);
-            String preuUnitatString = s.next();
-            try {
-                double preuUnitat = Double.parseDouble(preuUnitatString);
-                System.out.print(AMARILLO + "\n> Quantitat: " + RESET);
-                String quantitatString = s.next();
-                int quantitat = Integer.parseInt(quantitatString);
-                double subtotal=quantitat*preuUnitat;
-                System.out.print(AMARILLO + "\n> Vols afegir un altre producte? (s/n): " + RESET);
-                r = s.next();
-                comanda=comanda+"\n"+producte+"\t"+quantitatString+"\t\t"+preuUnitatString+"\t\t"+subtotal;
-            } catch (InputMismatchException e) {
-                System.out.println(ROJO+"\n(!) Sisplau introdueix un número vàlid"+RESET);
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println(ROJO+"\n(!) Sisplau introdueix un número vàlid"+RESET);
-                break;
-            } catch (Exception e) {
-                System.out.println(ROJO+"\nERROR DESCONEGUT: "+e+RESET);
-                break;
+        comanda=afegirProductes(comanda);
+        return comanda;
+    }
+
+    public String afegirProductes(String comanda) {
+        if (!comanda.equals("")) {
+            Boolean comandaCompletada=false;
+            String producte;
+            do {
+                s.nextLine();
+                producte = producteFormatat();
+                
+                try {
+                    System.out.print(AMARILLO + "\n> Preu unitari (€): " + RESET);
+                    String preuUnitatString = s.next();
+                    double preuUnitat = Double.parseDouble(preuUnitatString);
+                    System.out.print(AMARILLO + "\n> Quantitat: " + RESET);
+                    String quantitatString = s.next();
+                    int quantitat = Integer.parseInt(quantitatString);
+                    double subtotal=quantitat*preuUnitat;
+                    totalProductes=totalProductes+subtotal;
+                    IVAperAplicar=totalProductes*0.10;
+                    totalNet=totalProductes+IVAperAplicar;
+
+                    System.out.print(AMARILLO + "\n> Vols afegir un altre producte? (s/n): " + RESET);
+                    r = s.next();
+                    comanda=comanda+"\n"+producte+"\t"+quantitat+"\t\t"+String.format("%.2f", preuUnitat)+"\t\t"+String.format("%.2f", subtotal);
+                    comandaCompletada=true;
+                } catch (InputMismatchException e) {
+                    System.out.println(ROJO+"\n(!) Sisplau introdueix un número vàlid"+RESET);
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println(ROJO+"\n(!) Sisplau introdueix un número vàlid"+RESET);
+                    break;
+                } catch (Exception e) {
+                    System.out.println(ROJO+"\nERROR DESCONEGUT: "+e+RESET);
+                    break;
+                }
+            } while (!r.equalsIgnoreCase("n"));
+            if (comandaCompletada) {
+                imprimirTiquet(comanda);
             }
-        } while (!r.equalsIgnoreCase("n"));
-        System.out.println(AMARILLO+"\nS'està generant el tiquet..."+RESET);
-        imprimirTiquet(comanda);
+        } else {
+            System.out.println(ROJO+"(!) Sis plau primer crea una comanda per utilitzar això.\n"+RESET);
+        }
         return comanda;
     }
 
     public void imprimirTiquet(String comanda) {
-        System.out.println(comanda);
+        if (comanda.equals("")) {
+            System.out.println(ROJO+"(!) Sis plau primer crea una comanda per utilitzar això.\n"+RESET);
+        } else {
+            comanda=comanda+"\n------------------------------------------------------------------------"+"\nTotal sense IVA:\t\t\t\t\t"+String.format("%.2f", totalProductes)+"\nIVA (10%):      \t\t\t\t\t"+String.format("%.2f", IVAperAplicar)+"\nTOTAL A PAGAR:  \t\t\t\t\t"+String.format("%.2f", totalNet)+"\n========================================================================\n";
+            System.out.println(AMARILLO+"\nS'està generant el tiquet..."+RESET);
+            System.out.println(comanda);
+        }
     }
 
     public String producteFormatat() {
@@ -104,7 +131,6 @@ public class PE05_AcarretaAdrian {
                 int limit = 16-r.length();
                 for (int i=0;i<=limit;i++) {
                     nomFormatat=nomFormatat+" ";
-                    System.out.println(nomFormatat.length());
                 }
                 validName=true;
             } else {
